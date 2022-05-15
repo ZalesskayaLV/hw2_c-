e64803c940fed66c78adeed6a7b050b5ea4b6ecd
Lyubov Zalesskaya <zalesskayalyubov@gmail.com>
2022-05-16 00:33:34 +0400

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+ 645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/ 
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int exam = (num/100);
+
+if (exam<1) Console.WriteLine($"{num} -> третьей цифры нет");
+
+else 
+{
+    string digit = num.ToString();
+    Console.WriteLine($"{num} -> {digit[2]}");
+}
