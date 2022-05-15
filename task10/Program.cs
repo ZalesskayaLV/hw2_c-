2ef7fe97b403ca94de227300a7010ec8f8a43854
Lyubov Zalesskaya <zalesskayalyubov@gmail.com>
2022-05-15 23:41:04 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string digit = number.ToString();
+
+Console.WriteLine($"{number} -> {digit[1]}");
